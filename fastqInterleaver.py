84ae62f3d27f829ab3a16371c3b8483cbfcdeca4
Joel Sharbrough <jsharbrough@users.noreply.github.com>
2017-08-24 14:34:23 -0600

    Add files via upload

--- fastqInterleaver.py
+++ fastqInterleaver.py
@@ -1,10 +1,11 @@
 import sys
 def fastqInterleaver(forwardReads,reverseReads):
-    '''Takes two paired-end fastq read files as input and interleaves them. Only paired 
-    reads will be retained in the     output fastq. The program will interleave paired 
-    forward and reverse reads and write them to standard out, listing forward reads 
-    first in the output file to conform with convention. To run, open a Unix 
-    environment and type the following:
+    '''Takes two paired-end fastq read files as input (preferably paired-end 
+    reads) and interleaves them. Only paired reads will be retained in the 
+    output fastq. The program will interleave paired forward and reverse reads 
+    and write them to standard out, listing forward reads first in the output 
+    file to conform with convention.To run, open a Unix environment and type the 
+    following:
     
     python fastqInterleaver.py forwardReads reverseReads > out.fastq
     
@@ -26,7 +27,7 @@ def fastqInterleaver(forwardReads,reverseReads):
         if lineNum == 0:
             if seqName != '':
                 seqDict[seqName] = [currSeq]
-            lineSplit = line.split(' ')
+            lineSplit = line.split('/')
             seqName = lineSplit[0]
             currSeq = line
             lineNum += 1
@@ -39,13 +40,12 @@ def fastqInterleaver(forwardReads,reverseReads):
         elif lineNum == 3:
             currSeq += line
             lineNum = 0
-    seqDict[seqName] = currSeq
+    seqDict[seqName] = [currSeq]
     infile1.close()
     seqName = ''
     currSeq = ''
     infile2 = open(reverseReads,'r')
     lineNum = 0
-    readFileName = forwardReads[0:-7]
     for line in infile2:
         if lineNum == 0:
             if seqName != '' and seqName in seqDict:
@@ -53,7 +53,7 @@ def fastqInterleaver(forwardReads,reverseReads):
                 sys.stdout.write(seqList[0])
                 sys.stdout.write(currSeq)
                 del seqDict[seqName]
-            lineSplit = line.split(' ')
+            lineSplit = line.split('/')
             seqName = lineSplit[0]
             currSeq = line
             lineNum += 1
